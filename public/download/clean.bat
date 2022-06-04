4c959b3227d568c01ad7cbf3a451373f64f0d493
bonina <bonina@users.noreply.github.com>
2022-06-04 10:27:12 -0400

    Update clean.bat

--- public/download/clean.bat
+++ public/download/clean.bat
@@ -1,32 +1,40 @@
-::===============================================================
-:: Unnecessary files removal tool v1.1
-:: Windows 7/8/10/11
-:: @bonina.eu
-::===============================================================
 @echo off
+echo =====================================
+echo  Unnecessary files removal tool v1.1
+echo  for Windows 7/8/10/11
+::    @bonina.eu
+echo =====================================
+echo.
+
 net session >nul 2>&1
 if %errorLevel% == 0 (
 goto :start
 ) else (
-color 0C
 echo ERROR please execute with admin privileges
 goto :error
 )
 
 :start
-cleanmgr.exe /SAGESET:1 && (color 0A & echo Clean setup OK) || (color 0C & echo Clean setup ERROR & goto :error)
-cleanmgr.exe /SAGERUN:1 && (color 0A & echo. & echo Cleaning OK) || (color 0C & echo. & echo Cleaning ERROR & goto :error)
-Dism.exe /online /Cleanup-Image /StartComponentCleanup && (color 0A & echo Image cleanup OK) || (color 0C & echo Image cleanup ERROR & goto :error)
-Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase && (color 0A & echo Base image OK) || (color 0C & echo Base image ERROR & goto :error)
+color 0A
+echo Cleanup setup in progress... & cleanmgr.exe /SAGESET:1 && (echo Cleanup setup OK) || (echo Cleanup setup ERROR & goto :error)
+echo. & echo Cleanup in progress... & cleanmgr.exe /SAGERUN:1 && (echo Cleanup OK) || (echo Cleanup ERROR & goto :error)
+Dism.exe /online /Cleanup-Image /StartComponentCleanup && (echo WImage cleanup OK) || (echo Image cleanup ERROR & goto :error)
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase && (echo Base wimage cleanup OK) || (echo Base wimage cleanup ERROR & goto :error)
 
-del /S /Q /F "C:\Windows\SoftwareDistribution\Download\*" >nul && for /D %%p in (C:\Windows\SoftwareDistribution\Download\*) do rmdir /S /Q "%%p" && (color 0A & echo. & echo WUpdate cache OK & goto :success) || (color 0C & echo. & echo WUpdate cache ERROR & goto :error)
+echo. & echo WUpdate cache cleanup in progress... & (del /S /Q /F "C:\Windows\SoftwareDistribution\Download\*" >nul & for /D %%p in (C:\Windows\SoftwareDistribution\Download\*) do rmdir /S /Q "%%p") && (echo WUpdate cache cleanup OK & goto :success) || (echo WUpdate cache cleanup ERROR & goto :error)
 
 :success
 echo.
 echo.
-echo Unnecessary files sucessfuly removed
-:error
+echo ==========================================
+echo  ALL Unnecessary files sucessfuly removed
+echo ==========================================
+:continue
 echo.
 echo PRESS ANY KEY TO CLOSE WINDOW
 pause >nul
 goto: eof
+
+:error
+color 0C
+goto :continue
